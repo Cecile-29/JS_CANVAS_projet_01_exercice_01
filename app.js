2897c2acd11edcd339bc72d946f19ea4c73fd540
Cecile-29 <cecile.auger.pro@gmail.com>
2021-05-06 23:30:51 +0200

    push exercice_01 après correction

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// var toit = document.getElementById('house').getContext('2d');
-var toit = document.getElementById('house');
-var ctx = toit.getContext('2d');
-
+// var maison = document.getElementById('house').getContext('2d');
+var maison = document.getElementById('house');
+var ctx = maison.getContext('2d');
 ctx.fillStyle = '#D7823E';
 ctx.beginPath();
 ctx.moveTo(150, 20);
@@ -10,12 +9,10 @@ ctx.lineTo(280, 110);
 ctx.lineTo(150, 20);
 ctx.fill(); // Remplissage du dernier chemin tracé
 ctx.stroke();
-
 // murs maison
 ctx.fillStyle = '#a1d8e6';
 ctx.fillRect(20, 110, 260, 170);
 ctx.stroke();
-
 // porte
 ctx.fillStyle = '#c9c9c9';
 ctx.beginPath();
@@ -25,7 +22,6 @@ ctx.lineTo(170, 220);
 ctx.lineTo(170, 280);
 ctx.fill();
 ctx.stroke();
-
 // fenêtre droite
 ctx.fillStyle = '#c9c9c9';
 ctx.beginPath();
@@ -36,7 +32,6 @@ ctx.lineTo(90, 130);
 ctx.lineTo(50, 130);
 ctx.fill();
 ctx.stroke();
-
 // fenêtre gauche
 ctx.fillStyle = '#c9c9c9';
 ctx.beginPath();
